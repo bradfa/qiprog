c5c77c555db678efb9982b1f0bca67c721e32386
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-06-20 19:19:39 -0500

    libqiprog: Implement redirection wrappers for defined function
    For the QiProg functions which are defined, implement the thin redirection
    wrapper (dev->drv->function()).
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- libqiprog/src/core.c
+++ libqiprog/src/core.c
@@ -24,7 +24,6 @@
 
 #include "qiprog_internal.h"
 
-
 /**
  * @ingroup discovery
  */
@@ -42,8 +41,8 @@ qiprog_err qiprog_open_device(struct qiprog_device *dev)
 /**
  * @brief
  */
-qiprog_err qiprog_get_capabilities(struct qiprog_device *dev,
-				   struct qiprog_capabilities *caps)
+qiprog_err qiprog_get_capabilities(struct qiprog_device * dev,
+				   struct qiprog_capabilities * caps)
 {
 	QIPROG_RETURN_ON_BAD_DEV(dev);
 	return dev->drv->get_capabilities(dev, caps);
@@ -70,6 +69,118 @@ qiprog_err qiprog_set_bus(struct qiprog_device * dev, enum qiprog_bus bus)
 {
 	QIPROG_RETURN_ON_BAD_DEV(dev);
 	return dev->drv->set_bus(dev, bus);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_set_clock(struct qiprog_device * dev, uint32_t * clock_khz)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->set_clock(dev, clock_khz);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_read_chip_id(struct qiprog_device * dev,
+			       struct qiprog_chip_id ids[9])
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->read_chip_id(dev, ids);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_set_address(struct qiprog_device * dev, uint32_t start,
+			      uint32_t end)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->set_address(dev, start, end);
+}
+
+/* TODO: qiprog_set_erase_size */
+/* TODO: qiprog_set_erase_command */
+/* TODO: qiprog_set_write_command */
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_set_spi_timing(struct qiprog_device * dev,
+				 uint16_t tpu_read_us, uint32_t tces_ns)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->set_spi_timing(dev, tpu_read_us, tces_ns);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_read8(struct qiprog_device * dev, uint32_t addr,
+			uint8_t * data)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->read8(dev, addr, data);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_read16(struct qiprog_device * dev, uint32_t addr,
+			 uint16_t * data)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->read16(dev, addr, data);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_read32(struct qiprog_device * dev, uint32_t addr,
+			 uint32_t * data)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->read32(dev, addr, data);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_write8(struct qiprog_device * dev, uint32_t addr,
+			 uint8_t data)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->write8(dev, addr, data);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_write16(struct qiprog_device * dev, uint32_t addr,
+			  uint16_t data)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->write16(dev, addr, data);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_write32(struct qiprog_device * dev, uint32_t addr,
+			  uint32_t data)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->write32(dev, addr, data);
+}
+
+/**
+ * @brief
+ */
+qiprog_err qiprog_set_vdd(struct qiprog_device * dev, uint16_t vdd_mv)
+{
+	QIPROG_RETURN_ON_BAD_DEV(dev);
+	return dev->drv->set_vdd(dev, vdd_mv);
 }
 
 /** @} */

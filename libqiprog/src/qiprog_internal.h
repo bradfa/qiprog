a18efdac7aceef7da2005093fc49e715d4506a14
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-06-25 23:28:10 -0500

    libqiprog: Add endian-conversion helpers
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- libqiprog/src/qiprog_internal.h
+++ libqiprog/src/qiprog_internal.h
@@ -102,6 +102,79 @@ struct qiprog_device {
 	void *priv;
 };
 
+/**
+ * @defgroup endian_helpers QiProg byte-order conversion helpers
+ *
+ * @ingroup qiprog_doxygen_TODO
+ *
+ * @brief <b>QiProg byte-order conversion helpers</b>
+ *
+ * Since QiProg drivers need to communicate with the outside world, the byte
+ * ordering of "outside world" devices needs to be accounted for. In QiProg
+ * terms, outside data is a stream of bytes, whereas internal data is organized
+ * in data structures. Outside data never comes in structures, and internal data
+ * is never a plain byte stream. With this philosophy QiProg code does not, and
+ * must not care about host endianess.
+ *
+ * Internal data must always be inserted to or extracted from a data stream.
+ * Endian conversion macros, or byte-swaps must never be used. Instead, always
+ * use these functions.
+ */
+/** @{ */
+/**
+ * @brief Extract a double-word from a little-endian data stream
+ *
+ * @param src Pointer to 16-bit value in the LE data stream
+ * @return 16-bit value in host-endian order
+ */
+inline static uint16_t le16_to_h(void *src)
+{
+	uint8_t *b = src;
+	return ((b[1] << 8) | (b[0] << 0));
+};
+
+/**
+ * @brief Extract a long-word from a little-endian data stream
+ *
+ * @param src Pointer to 32-bit value in the LE data stream
+ * @return 32-bit value in host-endian order
+ */
+inline static uint32_t le32_to_h(void *src)
+{
+	uint8_t *b = src;
+	return ((b[3] << 24) | (b[2] << 16) | (b[1] << 8) | (b[0] << 0));
+};
+
+/**
+ * @brief Insert a double-word into a little-endian data stream
+ *
+ * @param val16 16-bit value to insert in the data stream
+ * @param dest Location in the data stream where the value should be inserted
+ */
+inline static void h_to_le16(uint16_t val16, void *dest)
+{
+	uint8_t *b = dest;
+	b[0] = (val16 >> 0) & 0xff;
+	b[1] = (val16 >> 8) & 0xff;
+};
+
+/**
+ * @brief Insert a long-word into a little-endian data stream
+ *
+ * @param val32 32-bit value to insert in the data stream
+ * @param dest Location in the data stream where the value should be inserted
+ */
+inline static void h_to_le32(uint16_t val32, void *dest)
+{
+	uint8_t *b = dest;
+	b[0] = (val32 >> 0) & 0xff;
+	b[1] = (val32 >> 8) & 0xff;
+	b[2] = (val32 >> 16) & 0xff;
+	b[3] = (val32 >> 24) & 0xff;
+};
+
+/** @} */
+
 /* util.c */
 qiprog_err dev_list_init(struct dev_list *list);
 qiprog_err dev_list_free(struct dev_list *list);

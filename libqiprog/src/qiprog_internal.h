bfe44f979ab6becad7257e28aabdfe916dd7cffe
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2013-07-02 23:10:46 -0500

    libqiprog: Add macros for MIN and MAX
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- libqiprog/src/qiprog_internal.h
+++ libqiprog/src/qiprog_internal.h
@@ -27,6 +27,20 @@
 
 #include <qiprog.h>
 
+#define MAX(a,b) \
+	({ \
+		__typeof__ (a) _a = (a);	\
+		__typeof__ (b) _b = (b);	\
+		_a > _b ? _a : _b;		\
+	})
+
+#define MIN(a,b) \
+	({ \
+		__typeof__ (a) _a = (a);	\
+		__typeof__ (b) _b = (b);	\
+		_a < _b ? _a : _b;		\
+	})
+
 #define QIPROG_RETURN_ON_BAD_DEV(dev) do {	\
 	if (dev == NULL)			\
 		return QIPROG_ERR_ARG;		\
